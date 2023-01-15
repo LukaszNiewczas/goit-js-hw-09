662989108b36bb1822d93abd8cc7a2566087fb29
LukaszNiewczas <niewczas1996@gmail.com>
2023-01-15 16:15:03 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let interval = null;
+
+startBtn.addEventListener('click', startChange);
+stopBtn.addEventListener('click', stopChange);
+
+function startChange() {
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+}
+
+function stopChange() {
+  clearInterval(interval);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+}
