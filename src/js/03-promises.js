bf94b830d603e705691a7eededf63fc48118ba9e
LukaszNiewczas <niewczas1996@gmail.com>
2023-01-28 17:13:22 +0100

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify as Notifly } from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const generatePromises = (delay, step, amount) => {
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, delay + step * i)
+      .then(value =>
+        Notifly.success(
+          `Fulfilled promise ${value.position + 1} in ${value.delay}ms`
+        )
+      )
+      .catch(error =>
+        Notifly.failure(
+          `Rejected promise ${error.position + 1} in ${error.delay}ms`
+        )
+      );
+  }
+};
+
+const submitButton = document.querySelector('[type="submit"]');
+submitButton.addEventListener('click', e => {
+  e.preventDefault();
+  const firstDelay = Number(document.querySelector('[name="delay"]').value);
+  const delayStep = Number(document.querySelector('[name="step"]').value);
+  const promiseAmount = Number(document.querySelector('[name="amount"]').value);
+  generatePromises(firstDelay, delayStep, promiseAmount);
+});
